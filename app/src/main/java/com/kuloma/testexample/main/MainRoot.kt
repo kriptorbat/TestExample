d6030bdccbd0c6d22ff73a941e2829487d4d36a0
kriptorbat <stasan.asanov@gmail.com>
2024-12-09 16:31:34 +0300

    add floating action button

--- app/src/main/java/com/kuloma/testexample/main/MainRoot.kt
+++ app/src/main/java/com/kuloma/testexample/main/MainRoot.kt
@@ -1,27 +1,18 @@
 package com.kuloma.testexample.main
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,14 +21,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import com.applandeo.materialcalendarview.CalendarDay
-import com.applandeo.materialcalendarview.CalendarView
-import com.applandeo.materialcalendarview.listeners.OnCalendarDayClickListener
-import com.kuloma.testexample.R
+
 import com.kuloma.testexample.ToDoEntity
 import com.kuloma.testexample.main.ui.CalendarScreen
 import com.kuloma.testexample.main.ui.ToDoListScreen
@@ -80,17 +65,32 @@ fun MainRoot(viewModel: MainViewModel){
     listToDo.add(d1)
     listToDo.add(d2)
     listToDo.add(d3)
-    Column {
-        CalendarScreen(
-            toDateClick = { date ->
-                chainedDate = date
+    Box{
+        Column {
+            CalendarScreen(
+                toDateClick = { date ->
+                    chainedDate = date
+                },
+                datesWithToDo = items
+            )
+            ToDoListScreen(
+                entities = listToDo,
+                toClickItem = {},
+                viewModel = viewModel
+            )
+        }
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomStart
+        ){
+            FloatingActionButton(onClick = {
+                Log.d("fab", "hi")
             },
-            datesWithToDo = items
-        )
-        ToDoListScreen(
-            entities = listToDo,
-            toClickItem = {},
-            viewModel = viewModel
-        )
+                modifier = Modifier.padding(20.dp),
+                containerColor = Blue,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Add")
+            }
+        }
     }
 }
